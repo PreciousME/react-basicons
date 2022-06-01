e7115e07610078582216fe50db3ad13e46cb2480
Solomon Omojola <omojolasolomon@gmail.com>
2022-06-01 18:21:40 +0200

    add figma & github

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
+import YuanCurrencyMoney from './icons/YuanCurrencyMoney'
 import ActivityGraph from './icons/ActivityGraph'
 import SnapchatSocial from './icons/SnapchatSocial'
 import XCircleCloseDelete from './icons/XCircleCloseDelete'
 import EducationSchool from './icons/EducationSchool'
 import Print from './icons/Print'
 import SecurityShieldPrivacyBad from './icons/SecurityShieldPrivacyBad'
+import BrowserChrome from './icons/BrowserChrome'
+import SketchDiamond from './icons/SketchDiamond'
 import WifiConnection from './icons/WifiConnection'
+import PlanetSaturnSpace from './icons/PlanetSaturnSpace'
+import Mouse from './icons/Mouse'
 import ShoppingCart from './icons/ShoppingCart'
+import PoundCurrencyMoney from './icons/PoundCurrencyMoney'
 import SettingsAccountMore from './icons/SettingsAccountMore'
+import MicrophoneOffMusicTalk from './icons/MicrophoneOffMusicTalk'
 import LockOpen from './icons/LockOpen'
+import SnowflakesWeatherCold from './icons/SnowflakesWeatherCold'
 import AirportPlaneTravel from './icons/AirportPlaneTravel'
 import GlobeWorldEarth from './icons/GlobeWorldEarth'
 import AlertOctagonError from './icons/AlertOctagonError'
@@ -16,58 +24,78 @@ import Forward from './icons/Forward'
 import CopyDuplicate from './icons/CopyDuplicate'
 import Pause from './icons/Pause'
 import Rewind from './icons/Rewind'
+import AccountUserPersonRound from './icons/AccountUserPersonRound'
 import Error from './icons/Error'
 import Shuffle from './icons/Shuffle'
 import AdjustVeticalSettings from './icons/AdjustVeticalSettings'
 import DownloadCloud from './icons/DownloadCloud'
 import Cloud from './icons/Cloud'
+import CaretSortSelectArrow from './icons/CaretSortSelectArrow'
 import AlertCirlcleError from './icons/AlertCirlcleError'
 import RocketLaunch from './icons/RocketLaunch'
 import ThunderstormWeather from './icons/ThunderstormWeather'
 import LaptopComputer from './icons/LaptopComputer'
 import LogoutExit from './icons/LogoutExit'
 import ArrowLeft from './icons/ArrowLeft'
+import FocusCameraPlusAdd from './icons/FocusCameraPlusAdd'
 import TiktokSocial from './icons/TiktokSocial'
 import FilterAlt from './icons/FilterAlt'
 import BedDoubleHotel from './icons/BedDoubleHotel'
+import EmojiSmileyAngryMad from './icons/EmojiSmileyAngryMad'
 import YoutubeVideo from './icons/YoutubeVideo'
 import Filter from './icons/Filter'
 import GenderFemale from './icons/GenderFemale'
 import SecurityPasswordKey from './icons/SecurityPasswordKey'
+import CompressArrowsConverge from './icons/CompressArrowsConverge'
 import Undo from './icons/Undo'
 import EditWriteAlt from './icons/EditWriteAlt'
 import EyePasswordShow from './icons/EyePasswordShow'
 import HelpQuestion from './icons/HelpQuestion'
 import ArrowUp from './icons/ArrowUp'
 import SecurityShield from './icons/SecurityShield'
+import EmojiSmileyDizzyHappy from './icons/EmojiSmileyDizzyHappy'
 import FacebookSocial from './icons/FacebookSocial'
 import MoreVertical from './icons/MoreVertical'
 import MastercardCreditDebit from './icons/MastercardCreditDebit'
 import Car from './icons/Car'
 import Gift from './icons/Gift'
+import UpTurnArrow from './icons/UpTurnArrow'
 import SecurityShieldAlt from './icons/SecurityShieldAlt'
 import CommentSquareAltChatMessage from './icons/CommentSquareAltChatMessage'
 import TramTrainTransport from './icons/TramTrainTransport'
 import TrashAltDeleteBin from './icons/TrashAltDeleteBin'
+import EmojiSmileySadFace from './icons/EmojiSmileySadFace'
 import Delete from './icons/Delete'
 import DownloadAlt from './icons/DownloadAlt'
 import MobilePhone from './icons/MobilePhone'
+import EmojiSmileyHappyFace from './icons/EmojiSmileyHappyFace'
 import ReplyAll from './icons/ReplyAll'
+import SparkleStar from './icons/SparkleStar'
 import CameraPhoto from './icons/CameraPhoto'
+import CheckBox from './icons/CheckBox'
 import HomeHouse from './icons/HomeHouse'
 import MoonNight from './icons/MoonNight'
+import FireTrendHot from './icons/FireTrendHot'
+import EmojiSmileyMehSad from './icons/EmojiSmileyMehSad'
 import MinimizeCollapse from './icons/MinimizeCollapse'
+import AttachmentClipHook from './icons/AttachmentClipHook'
 import TwitterSocialTweet from './icons/TwitterSocialTweet'
+import CrosshairTargetCenter from './icons/CrosshairTargetCenter'
 import UserAccountProfile from './icons/UserAccountProfile'
 import EuroCurrency from './icons/EuroCurrency'
+import ExpandArrows from './icons/ExpandArrows'
 import UserAddAccountProfile from './icons/UserAddAccountProfile'
+import DesktopComputerMac from './icons/DesktopComputerMac'
+import DesktopMacComputer from './icons/DesktopMacComputer'
 import HeartLoveLike from './icons/HeartLoveLike'
 import PlugConnect from './icons/PlugConnect'
 import CommentCircleAltChatMessage from './icons/CommentCircleAltChatMessage'
 import UploadCloud from './icons/UploadCloud'
+import DownTurnArrow from './icons/DownTurnArrow'
 import BranchGitFork from './icons/BranchGitFork'
 import NotificationOffBellAlarm from './icons/NotificationOffBellAlarm'
 import SubtractCircleMinusRemove from './icons/SubtractCircleMinusRemove'
+import FaceId from './icons/FaceId'
 import MenuHambuger from './icons/MenuHambuger'
 import StarRatings from './icons/StarRatings'
 import PaperFileText from './icons/PaperFileText'
@@ -87,6 +115,7 @@ import HealthCrossFirstaid from './icons/HealthCrossFirstaid'
 import CursorAltPointerMouse from './icons/CursorAltPointerMouse'
 import Redo from './icons/Redo'
 import BatteryEmpty from './icons/BatteryEmpty'
+import MarkerCreatePen from './icons/MarkerCreatePen'
 import ToogleLeft from './icons/ToogleLeft'
 import PhotoImagePicture from './icons/PhotoImagePicture'
 import PhoneCall from './icons/PhoneCall'
@@ -94,8 +123,10 @@ import ShuffleAlt from './icons/ShuffleAlt'
 import RoadmapTimeline from './icons/RoadmapTimeline'
 import HashtagTrendingNumber from './icons/HashtagTrendingNumber'
 import FaceMask from './icons/FaceMask'
+import HarddriveHarddisk from './icons/HarddriveHarddisk'
 import AdAnnouncementMegaphone from './icons/AdAnnouncementMegaphone'
 import Play from './icons/Play'
+import CameraApertureCaptureLens from './icons/CameraApertureCaptureLens'
 import ThumbsUp from './icons/ThumbsUp'
 import ZoomOut from './icons/ZoomOut'
 import AlertError from './icons/AlertError'
@@ -103,6 +134,7 @@ import GenderMale from './icons/GenderMale'
 import ShopStore from './icons/ShopStore'
 import Badge from './icons/Badge'
 import VolumeMute from './icons/VolumeMute'
+import NairaCurrencyMoney from './icons/NairaCurrencyMoney'
 import CutScissors from './icons/CutScissors'
 import Save from './icons/Save'
 import EditWrite from './icons/EditWrite'
@@ -112,11 +144,13 @@ import ToogleRight from './icons/ToogleRight'
 import LocationPinAlt from './icons/LocationPinAlt'
 import ModelDiamond from './icons/ModelDiamond'
 import UploadAlt from './icons/UploadAlt'
+import AccountUserPersonSquare from './icons/AccountUserPersonSquare'
 import ZoomIn from './icons/ZoomIn'
 import SunDay from './icons/SunDay'
 import Chatbot from './icons/Chatbot'
 import AsteriskStar from './icons/AsteriskStar'
 import LinkedinSocial from './icons/LinkedinSocial'
+import MicrophoneMusicTalk from './icons/MicrophoneMusicTalk'
 import Info from './icons/Info'
 import LaunchLinkOpen from './icons/LaunchLinkOpen'
 import ChevronDownArrow from './icons/ChevronDownArrow'
@@ -125,6 +159,7 @@ import Watch from './icons/Watch'
 import PieChart from './icons/PieChart'
 import Bluetooth from './icons/Bluetooth'
 import AnalyticsGraphChart from './icons/AnalyticsGraphChart'
+import EmojiAngelSmileyHappy from './icons/EmojiAngelSmileyHappy'
 import MusicNote from './icons/MusicNote'
 import BatteryFull from './icons/BatteryFull'
 import NotificationBellAlarm from './icons/NotificationBellAlarm'
@@ -136,6 +171,7 @@ import CourseDiary from './icons/CourseDiary'
 import Stop from './icons/Stop'
 import TagPriceDiscount from './icons/TagPriceDiscount'
 import EyePasswordHide from './icons/EyePasswordHide'
+import Gif from './icons/Gif'
 import EmailMessageInbox from './icons/EmailMessageInbox'
 import BasketballBallSports from './icons/BasketballBallSports'
 import CommentChatMessageAlt from './icons/CommentChatMessageAlt'
@@ -143,19 +179,25 @@ import Wallet from './icons/Wallet'
 import DollarCurrency from './icons/DollarCurrency'
 import BatteryCharging from './icons/BatteryCharging'
 import DragVertical from './icons/DragVertical'
+import CrosshairTargetCenterAlt from './icons/CrosshairTargetCenterAlt'
 import ChevronsLeftArrows from './icons/ChevronsLeftArrows'
 import UmbrellaInsurance from './icons/UmbrellaInsurance'
 import ArrowTopLeft from './icons/ArrowTopLeft'
+import CaretSortSelectArrowAlt from './icons/CaretSortSelectArrowAlt'
 import CoffeeTea from './icons/CoffeeTea'
 import Apps from './icons/Apps'
+import CalendarDateAppointmentTime from './icons/CalendarDateAppointmentTime'
+import MagicWand from './icons/MagicWand'
 import Pin from './icons/Pin'
 import XOctagonError from './icons/XOctagonError'
 import FaceMaskAlt from './icons/FaceMaskAlt'
 import Download from './icons/Download'
 import AddCirclePlus from './icons/AddCirclePlus'
 import Basicons from './icons/Basicons'
+import EmojiSmileySurprisedShockedAmazed from './icons/EmojiSmileySurprisedShockedAmazed'
 import ChevronRightArrow from './icons/ChevronRightArrow'
 import MapLocation from './icons/MapLocation'
+import BadgeVerified from './icons/BadgeVerified'
 import Archive from './icons/Archive'
 import ThumbsDown from './icons/ThumbsDown'
 import PreviousArrowBackward from './icons/PreviousArrowBackward'
@@ -168,6 +210,7 @@ import ArrowRight from './icons/ArrowRight'
 import SaveAlt from './icons/SaveAlt'
 import FlagCountry from './icons/FlagCountry'
 import WifiOffConnection from './icons/WifiOffConnection'
+import RightTurnArrow from './icons/RightTurnArrow'
 import ChevronLeftArrow from './icons/ChevronLeftArrow'
 import FaceMaskAlt2 from './icons/FaceMaskAlt2'
 import AdjustHorizontalSettings from './icons/AdjustHorizontalSettings'
@@ -175,24 +218,31 @@ import UserRemoveAccountProfile from './icons/UserRemoveAccountProfile'
 import BikerCycle from './icons/BikerCycle'
 import ShoppingBag from './icons/ShoppingBag'
 import CreditCardDebit from './icons/CreditCardDebit'
+import AccessibilityHandicapDisabilityHandicapWheelchair from './icons/AccessibilityHandicapDisabilityHandicapWheelchair'
 import XSquareCloseDelete from './icons/XSquareCloseDelete'
 import BookmarkSave from './icons/BookmarkSave'
 import NextArrowForward from './icons/NextArrowForward'
 import BookNotePaper from './icons/BookNotePaper'
+import EmojiSmileyWinkHappy from './icons/EmojiSmileyWinkHappy'
 import SubtractMinusRemove from './icons/SubtractMinusRemove'
 import FolderFileProject from './icons/FolderFileProject'
 import SendMessageDm from './icons/SendMessageDm'
+import AddPlusNewSquareBoxSquare from './icons/AddPlusNewSquareBoxSquare'
+import Puzzle from './icons/Puzzle'
 import FastFoward from './icons/FastFoward'
 import CommentChatMessage from './icons/CommentChatMessage'
 import LocationPin from './icons/LocationPin'
 import ChevronsRightArrows from './icons/ChevronsRightArrows'
+import MediaCast from './icons/MediaCast'
 import InstagramSocial from './icons/InstagramSocial'
 import FuelGasStation from './icons/FuelGasStation'
 import BankFinancial from './icons/BankFinancial'
 import Crop from './icons/Crop'
+import EmojiSmileyBaby from './icons/EmojiSmileyBaby'
+import Chip from './icons/Chip'
 import Usb from './icons/Usb'
+import Cone from './icons/Cone'
 import UserGroupAccounts from './icons/UserGroupAccounts'
-import Github from './icons/Github'
 import LoadingSpinner from './icons/LoadingSpinner'
 import ReceiptPayment from './icons/ReceiptPayment'
 import CameraOffPhoto from './icons/CameraOffPhoto'
@@ -200,57 +250,67 @@ import MoreHorizontal from './icons/MoreHorizontal'
 import BatteryHalf from './icons/BatteryHalf'
 import ClockTime from './icons/ClockTime'
 import XCloseDelete from './icons/XCloseDelete'
+import MedalWinnerGold from './icons/MedalWinnerGold'
 import PaperNote from './icons/PaperNote'
 import VolumeDown from './icons/VolumeDown'
+import CalculatorComputeMath from './icons/CalculatorComputeMath'
 import ArrowTopRight from './icons/ArrowTopRight'
 import CursorPointerMouse from './icons/CursorPointerMouse'
 import CommentCircleChatMessage from './icons/CommentCircleChatMessage'
 import CalendarAppointmentDate from './icons/CalendarAppointmentDate'
 import TicketPass from './icons/TicketPass'
 import Reply from './icons/Reply'
+import CalculatorComputeMathAlt from './icons/CalculatorComputeMathAlt'
 import VideoCameraMedia from './icons/VideoCameraMedia'
 import FolderAddFile from './icons/FolderAddFile'
 import MoneyCashCurrency from './icons/MoneyCashCurrency'
 import RainCloudWeather from './icons/RainCloudWeather'
 import CodeTechDev from './icons/CodeTechDev'
 import BluetoothOff from './icons/BluetoothOff'
+import EnterArrowLeft from './icons/EnterArrowLeft'
 import ShoppingAltBag from './icons/ShoppingAltBag'
 import EntryLogin from './icons/EntryLogin'
-import Figma from './icons/Figma'
 import ArrowDown from './icons/ArrowDown'
 import ArrowBottomRight from './icons/ArrowBottomRight'
 import ChevronUpArrow from './icons/ChevronUpArrow'
+import SquareBoxUncheck from './icons/SquareBoxUncheck'
 import Search from './icons/Search'
 import WorkBag from './icons/WorkBag'
+import DiscountPercent from './icons/DiscountPercent'
 import LockPrivacy from './icons/LockPrivacy'
 import HospitalClinic from './icons/HospitalClinic'
 import ArrowBottomLeft from './icons/ArrowBottomLeft'
-export {
+const Icons: { [key: string]: React.FC<IconConfig> } = {
     ArrowBottomLeft,
     HospitalClinic,
     LockPrivacy,
+    DiscountPercent,
     WorkBag,
     Search,
+    SquareBoxUncheck,
     ChevronUpArrow,
     ArrowBottomRight,
     ArrowDown,
-    Figma,
     EntryLogin,
     ShoppingAltBag,
+    EnterArrowLeft,
     BluetoothOff,
     CodeTechDev,
     RainCloudWeather,
     MoneyCashCurrency,
     FolderAddFile,
     VideoCameraMedia,
+    CalculatorComputeMathAlt,
     Reply,
     TicketPass,
     CalendarAppointmentDate,
     CommentCircleChatMessage,
     CursorPointerMouse,
     ArrowTopRight,
+    CalculatorComputeMath,
     VolumeDown,
     PaperNote,
+    MedalWinnerGold,
     XCloseDelete,
     ClockTime,
     BatteryHalf,
@@ -258,24 +318,31 @@ export {
     CameraOffPhoto,
     ReceiptPayment,
     LoadingSpinner,
-    Github,
     UserGroupAccounts,
+    Cone,
     Usb,
+    Chip,
+    EmojiSmileyBaby,
     Crop,
     BankFinancial,
     FuelGasStation,
     InstagramSocial,
+    MediaCast,
     ChevronsRightArrows,
     LocationPin,
     CommentChatMessage,
     FastFoward,
+    Puzzle,
+    AddPlusNewSquareBoxSquare,
     SendMessageDm,
     FolderFileProject,
     SubtractMinusRemove,
+    EmojiSmileyWinkHappy,
     BookNotePaper,
     NextArrowForward,
     BookmarkSave,
     XSquareCloseDelete,
+    AccessibilityHandicapDisabilityHandicapWheelchair,
     CreditCardDebit,
     ShoppingBag,
     BikerCycle,
@@ -283,6 +350,7 @@ export {
     AdjustHorizontalSettings,
     FaceMaskAlt2,
     ChevronLeftArrow,
+    RightTurnArrow,
     WifiOffConnection,
     FlagCountry,
     SaveAlt,
@@ -295,19 +363,25 @@ export {
     PreviousArrowBackward,
     ThumbsDown,
     Archive,
+    BadgeVerified,
     MapLocation,
     ChevronRightArrow,
+    EmojiSmileySurprisedShockedAmazed,
     Basicons,
     AddCirclePlus,
     Download,
     FaceMaskAlt,
     XOctagonError,
     Pin,
+    MagicWand,
+    CalendarDateAppointmentTime,
     Apps,
     CoffeeTea,
+    CaretSortSelectArrowAlt,
     ArrowTopLeft,
     UmbrellaInsurance,
     ChevronsLeftArrows,
+    CrosshairTargetCenterAlt,
     DragVertical,
     BatteryCharging,
     DollarCurrency,
@@ -315,6 +389,7 @@ export {
     CommentChatMessageAlt,
     BasketballBallSports,
     EmailMessageInbox,
+    Gif,
     EyePasswordHide,
     TagPriceDiscount,
     Stop,
@@ -326,6 +401,7 @@ export {
     NotificationBellAlarm,
     BatteryFull,
     MusicNote,
+    EmojiAngelSmileyHappy,
     AnalyticsGraphChart,
     Bluetooth,
     PieChart,
@@ -334,11 +410,13 @@ export {
     ChevronDownArrow,
     LaunchLinkOpen,
     Info,
+    MicrophoneMusicTalk,
     LinkedinSocial,
     AsteriskStar,
     Chatbot,
     SunDay,
     ZoomIn,
+    AccountUserPersonSquare,
     UploadAlt,
     ModelDiamond,
     LocationPinAlt,
@@ -348,6 +426,7 @@ export {
     EditWrite,
     Save,
     CutScissors,
+    NairaCurrencyMoney,
     VolumeMute,
     Badge,
     ShopStore,
@@ -355,8 +434,10 @@ export {
     AlertError,
     ZoomOut,
     ThumbsUp,
+    CameraApertureCaptureLens,
     Play,
     AdAnnouncementMegaphone,
+    HarddriveHarddisk,
     FaceMask,
     HashtagTrendingNumber,
     RoadmapTimeline,
@@ -364,6 +445,7 @@ export {
     PhoneCall,
     PhotoImagePicture,
     ToogleLeft,
+    MarkerCreatePen,
     BatteryEmpty,
     Redo,
     CursorAltPointerMouse,
@@ -383,58 +465,78 @@ export {
     PaperFileText,
     StarRatings,
     MenuHambuger,
+    FaceId,
     SubtractCircleMinusRemove,
     NotificationOffBellAlarm,
     BranchGitFork,
+    DownTurnArrow,
     UploadCloud,
     CommentCircleAltChatMessage,
     PlugConnect,
     HeartLoveLike,
+    DesktopMacComputer,
+    DesktopComputerMac,
     UserAddAccountProfile,
+    ExpandArrows,
     EuroCurrency,
     UserAccountProfile,
+    CrosshairTargetCenter,
     TwitterSocialTweet,
+    AttachmentClipHook,
     MinimizeCollapse,
+    EmojiSmileyMehSad,
+    FireTrendHot,
     MoonNight,
     HomeHouse,
+    CheckBox,
     CameraPhoto,
+    SparkleStar,
     ReplyAll,
+    EmojiSmileyHappyFace,
     MobilePhone,
     DownloadAlt,
     Delete,
+    EmojiSmileySadFace,
     TrashAltDeleteBin,
     TramTrainTransport,
     CommentSquareAltChatMessage,
     SecurityShieldAlt,
+    UpTurnArrow,
     Gift,
     Car,
     MastercardCreditDebit,
     MoreVertical,
     FacebookSocial,
+    EmojiSmileyDizzyHappy,
     SecurityShield,
     ArrowUp,
     HelpQuestion,
     EyePasswordShow,
     EditWriteAlt,
     Undo,
+    CompressArrowsConverge,
     SecurityPasswordKey,
     GenderFemale,
     Filter,
     YoutubeVideo,
+    EmojiSmileyAngryMad,
     BedDoubleHotel,
     FilterAlt,
     TiktokSocial,
+    FocusCameraPlusAdd,
     ArrowLeft,
     LogoutExit,
     LaptopComputer,
     ThunderstormWeather,
     RocketLaunch,
     AlertCirlcleError,
+    CaretSortSelectArrow,
     Cloud,
     DownloadCloud,
     AdjustVeticalSettings,
     Shuffle,
     Error,
+    AccountUserPersonRound,
     Rewind,
     Pause,
     CopyDuplicate,
@@ -443,14 +545,23 @@ export {
     AlertOctagonError,
     GlobeWorldEarth,
     AirportPlaneTravel,
+    SnowflakesWeatherCold,
     LockOpen,
+    MicrophoneOffMusicTalk,
     SettingsAccountMore,
+    PoundCurrencyMoney,
     ShoppingCart,
+    Mouse,
+    PlanetSaturnSpace,
     WifiConnection,
+    SketchDiamond,
+    BrowserChrome,
     SecurityShieldPrivacyBad,
     Print,
     EducationSchool,
     XCircleCloseDelete,
     SnapchatSocial,
-    ActivityGraph
+    ActivityGraph,
+    YuanCurrencyMoney
 }
+export default Icons
